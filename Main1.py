f9decc75da3e6c8e2435ed541d259e92a6d5a2aa
enyangxxx <ineffab@gmail.com>
2019-04-20 18:34:24 +0200

    add new images for github

--- Main1.py
+++ Main1.py
@@ -13,13 +13,16 @@ import ANN_Functions as annf
 side_length = 100
 np_training_set_x, np_training_set_y = pp.load_dataset('training','jpg',side_length)
 np_validation_set_x, np_validation_set_y = pp.load_dataset('validation','jpg',side_length)
+np_test_set_x, np_test_set_y = pp.load_dataset('evaluation','jpg',side_length)
 np_training_set_x = annf.standardize(np_training_set_x)
 np_validation_set_x = annf.standardize(np_validation_set_x)
+np_test_set_x = annf.standardize(np_test_set_x)
 
 parameters = annm.L_layer_model(np_training_set_x, np_training_set_y, 
           num_iterations = 3000, learning_rate = 0.1, print_cost = True, 
           layers_dims= [side_length*side_length*3, 100, 80, 60, 40, 20, 10, 1])
 
 
-#np_evaluation_set_x, np_evaluation_set_y = pp.load_dataset('evaluation','jpg',100)
-Y_prediction_eva = annf.predict(np_validation_set_x,np_validation_set_y, parameters)
+Y_prediction_vali = annf.predict(np_validation_set_x,np_validation_set_y, parameters)
+Y_prediction_test = annf.predict(np_test_set_x,np_test_set_y, parameters)
+Y_prediction_train = annf.predict(np_training_set_x,np_training_set_y, parameters)
